d9bcab162c30e772b9b70d3a58313c5a0ee71397
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-18 16:41:57 +0200

    Add an abstract value that returns SleepDatabaseDao

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -22,4 +22,8 @@ import androidx.room.RoomDatabase
 @Database(entities = [SleepNight::class], version = 1,  exportSchema = false)
 abstract class SleepDatabase: RoomDatabase() {
 
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+    companion object{
+
+    }
 }
