9c3ace95ec492e2742c8eeb141a6ccbf9e0e1ba1
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-18 14:06:31 +0200

    Create SleepNight data class

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,16 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.Entity
+
+data class SleepNight(
+        var nightId: Long = 0L,
+
+        var startTimeMilli: Long = System.currentTimeMillis(),
+
+        var endTimeMilli: Long = startTimeMilli,
+
+        var sleepQuality: Int = -1
+
+)
