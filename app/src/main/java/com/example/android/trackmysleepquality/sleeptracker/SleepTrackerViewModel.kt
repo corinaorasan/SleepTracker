16e321d54fa8aec8fea9220b40dc5b4212cae30b
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-22 15:58:26 +0200

    Add onStopTracking() to handle click on Stop btn

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -65,11 +65,20 @@ class SleepTrackerViewModel(
         uiScope.launch {
             val newNight = SleepNight()
             insert(newNight)
-            tonight.value=getTonightFromDatabase()
+            tonight.value = getTonightFromDatabase()
         }
     }
 
-    private suspend fun insert(night: SleepNight){
+    fun onStopTracking(){
+        uiScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+
+    private suspend fun insert(night: SleepNight) {
         withContext(Dispatchers.IO) {
             database.insert(night)
         }
