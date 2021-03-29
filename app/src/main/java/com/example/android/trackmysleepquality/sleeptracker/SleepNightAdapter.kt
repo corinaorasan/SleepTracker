86c700f1d161aca17b55047683f1851b536d71ca
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-29 19:32:14 +0300

    Define data source

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -19,4 +19,6 @@ package com.example.android.trackmysleepquality.sleeptracker
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>()
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
+    var data= listOf<SleepNight>()
+}
