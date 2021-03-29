b80e61042f05c8aaa0804ed891faf52dcaef1897
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-29 19:37:56 +0300

    Override getItemCount to return data.size

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -18,7 +18,12 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
-    var data= listOf<SleepNight>()
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+
+    override fun getItemCount()=data.size
+
+    }
 }
