fa1b410edc11011e02f84b9847ced3af2be27a4c
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-30 09:41:39 +0300

    Override onBindViewHolder to return a list of numbers

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -20,10 +20,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+abstract class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
     var data = listOf<SleepNight>()
 
-    override fun getItemCount()=data.size
+    override fun getItemCount() = data.size
 
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text=item.sleepQuality.toString()
     }
 }
+
